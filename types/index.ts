1ee4809e406cf043bf00d6f53d669692c4236909
Dineth Sadeepa Edirisinghe <dnext2000@gmail.com>
2025-06-30 19:33:16 +0530

    Save my local changes before pulling

---
+++ types/index.ts
@@ -0,0 +1,64 @@
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+  title: string;
+  containerStyles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit" | "reset";
+  textStyles?: string;
+  rightIcon?: string;
+  isDisabled?: boolean;
+}
+
+export interface SearchManufacturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+  city_mpg: number;
+  class: number;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+}
+
+export interface FilterProps {
+  manufacturer: string;
+  year: number;
+  model: string;
+  fuel: string;
+  limit?: number;
+}
+
+export interface HomeProps {
+  searchParams: Promise<{
+    manufacturer?: string;
+    year?: string;
+    model?: string;
+    fuel?: string;
+    limit?: string;
+  }>;
+}
+
+export interface OptionProps {
+  title: string;
+  value: string;
+}
+
+export interface CustomFilterProps {
+  title: string;
+  options: OptionProps[];
+}
+
+export interface ShowMoreProps {
+  pageNumber: number;
+  isNext: boolean;
+}
